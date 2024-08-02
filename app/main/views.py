3826216d7582dc70681b3435487780ba8e677e94
Demon <you@awe.com>
2024-08-02 13:37:34 +0300

    'Main view(index) /'

--- app/main/views.py
+++ app/main/views.py
@@ -3,23 +3,23 @@ from flask import render_template, session, redirect, url_for, abort, flash, req
     current_app as app, make_response
 from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
-from ..models import User, Role, Permission
+from ..models import User, Role, Permission, Post
 from ..decorators import admin_required, permission_required
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form: NameForm = NameForm()
-    if form.validate_on_submit():
-        # ...
+    form: PostForm = PostForm()
+    if current_user.can(Permission.WRITE) and \
+        form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user._get_current_object()) 
+        db.session.add(post)
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
-    
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
+
 
 @main.route('/user/<username>')
 def user(username):
