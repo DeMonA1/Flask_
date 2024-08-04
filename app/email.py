db6c8836f561d01865370799a4b61f2b7b241b2d
Demon <you@awe.com>
2024-08-04 12:00:38 +0300

    'Corrected context'

--- app/email.py
+++ app/email.py
@@ -1,18 +1,18 @@
 from threading import Thread
-from flask import Flask, current_app as app, render_template
+from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
 
 
-def send_async_email(app: Flask, msg: Message):
-    with app.app_context():
-        mail.send(msg)
+def send_async_email(app_context, msg: Message):
+    app_context.push()
+    mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
+    thr = Thread(target=send_async_email, args=[current_app.app_context(), msg])
     thr.start()
     return thr
