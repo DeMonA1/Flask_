879cee35b75067f460972dfc6f2a9af5a2f6d6df
Demon <you@awe.com>
2024-08-05 15:43:50 +0300

    'edited'

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -10,13 +10,12 @@ auth = HTTPBasicAuth()
 @auth.verify_password
 def verify_password(email_or_token, password):
     if email_or_token == '':
-        g.current_user = AnonymousUser()
-        return True
+        return False
     if password == '':
         g.current_user = User.verify_auth_token(email_or_token)
         g.token_used = True
         return g.current_user is not None
-    user = User.query.filter_by(email_or_token=email_or_token).first()
+    user = User.query.filter_by(email=email_or_token.lower()).first()
     if not user:
         return False
     g.current_user = user
@@ -37,9 +36,8 @@ def before_request():
         return forbidden('Unconfirmed account')
     
     
-@api.route('/token')
+@api.route('/tokens/', method=['POST'])
 def get_token():
-    if g.current_user.is_anonymous() or g.token_used:
+    if g.current_user.is_anonymous or g.token_used:
         return unauthorized('Invalid credentials')
-    return jsonify({
-        'token': g.current_user.generate_auth_token()})
+    return jsonify({'token': g.current_user.generate_auth_token()})
