9bb10b44078646a6d1fb58752bffc3ef6cc261fa
Demon <you@awe.com>
2024-08-05 16:01:00 +0300

    'edited'

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,6 +1,6 @@
 from flask_httpauth import HTTPBasicAuth
 from flask import g, jsonify
-from ..models import User, AnonymousUser
+from ..models import User
 from . import api
 from .errors import unauthorized, forbidden
 
@@ -8,14 +8,14 @@ from .errors import unauthorized, forbidden
 auth = HTTPBasicAuth()
 
 @auth.verify_password
-def verify_password(email_or_token, password):
+def verify_password(email_or_token: str | bytes, password):
     if email_or_token == '':
         return False
     if password == '':
         g.current_user = User.verify_auth_token(email_or_token)
         g.token_used = True
         return g.current_user is not None
-    user = User.query.filter_by(email=email_or_token.lower()).first()
+    user: User = User.query.filter_by(email=email_or_token.lower()).first()
     if not user:
         return False
     g.current_user = user
@@ -36,7 +36,7 @@ def before_request():
         return forbidden('Unconfirmed account')
     
     
-@api.route('/tokens/', method=['POST'])
+@api.route('/tokens/', methods=['POST'])
 def get_token():
     if g.current_user.is_anonymous or g.token_used:
         return unauthorized('Invalid credentials')
