13eef48bd97c9caaac6259c49c6e8d2228ad0cc2
Demon <you@awe.com>
2024-08-05 08:22:03 +0300

    'RESTful blueprint'

--- app/__init__.py
+++ app/__init__.py
@@ -46,6 +46,9 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+    
     # Routes and non-standard error messages are connected here
     
     return app
