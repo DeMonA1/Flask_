375623cb10073938b3ac80b72af83e3044082cf9
Demon <you@awe.com>
2024-08-05 22:20:33 +0300

    'edited'

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 from app import create_app, db
 from app.models import User, Role, Permission, AnonymousUser, Follow
+from app.fake import users, posts
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -14,7 +15,6 @@ class UserModelTestCase(unittest.TestCase):
             db.create_all()
         Role.insert_roles()
         
-    
     def tearDown(self):
         db.session.remove()
         db.drop_all()
