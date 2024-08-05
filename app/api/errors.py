3127b5e9c72e2d6221d1ff5f695442942c8b5065
Demon <you@awe.com>
2024-08-05 11:47:48 +0300

    'Validation error'

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from . import api
+from ..exceptions import ValidationError
 
 
 def bad_request(message):
@@ -16,3 +17,7 @@ def forbidden(message):
     response = jsonify({'error': 'forbidden', 'message': message})
     response.status_code = 403
     return response
+
+@api.errorhandler(ValidationError)
+def validation_error(e):
+    return bad_request(e.args[0])
