c78901c508595413e22806e5a5a5853296795846
Demon <you@awe.com>
2024-08-08 19:35:23 +0300

    deploy (db, roles, followers)

--- manage.py
+++ manage.py
@@ -69,6 +69,18 @@ def profile(length, profile_dir):
         os.environ.pop('FLASK_RUN_FROM_CLI')
     app.run()
 
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+    
+    # create or update user roles
+    Role.insert_roles()
+    
+    # ensure all users are following themselves
+    User.add_self_follows()
     
 if __name__ == '__main__':
     app.run()
