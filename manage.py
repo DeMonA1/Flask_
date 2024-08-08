fd5275945fecc8d0225ab1affae5727afe97db46
Demon <you@awe.com>
2024-08-08 19:34:52 +0300

    profiler

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ import sys
 import click
 from app import create_app, db
 from app.models import User, Role
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -55,5 +55,20 @@ def test(coverage, test_names):
         COV.erase()
 
 
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """Start the application under the code profiler."""
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    if os.environ.get('FLASK_RUN_FROM_CLI'):
+        os.environ.pop('FLASK_RUN_FROM_CLI')
+    app.run()
+
+    
 if __name__ == '__main__':
     app.run()
